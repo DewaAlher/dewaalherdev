071ac6b592dd0d9401c5f566ce15e71f22097923
DewaAlher <59711776+DewaAlher@users.noreply.github.com>
2023-05-07 21:14:35 +0700

    idk

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  public greeting: string;
 
+  constructor() {
+    const hour = new Date().getHours();
+    if (hour < 12) {
+      this.greeting = 'Morning';
+    } else if (hour < 18) {
+      this.greeting = 'Afternoon';
+    } else {
+      this.greeting = 'Evening';
+    }
+  }
+
+  ngOnInit() {
+    // kode lain yang mungkin kamu tambahkan di sini
+  }
 }
