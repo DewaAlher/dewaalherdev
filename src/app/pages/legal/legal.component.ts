d56d0ae35b335ed2c82488056200e67b89a85ffc
DewaAlher <59711776+DewaAlher@users.noreply.github.com>
2023-05-07 20:38:17 +0700

    "finalize"

--- src/app/pages/legal/legal.component.ts
+++ src/app/pages/legal/legal.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-legal',
   templateUrl: './legal.component.html',
   styleUrls: ['./legal.component.scss'],
 })
-export class LegalComponent {
- 
+export class LegalComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const fragment = this.route.snapshot.fragment; 
+        if (fragment) {
+          const element = document.querySelector(`#${fragment}`); 
+          if (element) {
+            element.scrollIntoView({ behavior: 'smooth', block: 'start' }); 
+          }
+        }
+      });
+  }
 }
-
-
